ab0902ced7cb8235fd54c1c82b1005fffbdd2576
KavithRashintha <wgkrashintha@gmail.com>
2023-09-07 11:03:18 +0530

    LogIn Component Created

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import SignUp from "./components/signUp/signUp";
+import LogIn from "./components/logIn/logIn";
 
 function App() {
   return (
     <div className="App">
-      <SignUp></SignUp>
+      <LogIn/>
     </div>
   );
 }
